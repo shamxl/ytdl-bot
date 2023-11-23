d02e9d3cf6fa0abc194895c24518f42a37790d78
shamxl <shamxl@pm.me>
2023-11-23 13:49:45 +0530

    Fix bug

--- helpers/dl.py
+++ helpers/dl.py
@@ -2,13 +2,14 @@ from pytube import YouTube
 import io
 
 
-def download (url):
+async def download (url):
 
     buffer = io.BytesIO()
     yt = YouTube(url)
-    yt.streams.get_highest_resolution().stream_to_buffer(buffer)
-
+    video = yt.streams.get_highest_resolution()
+    buffer.name = video.title
+    video.stream_to_buffer(buffer)
+    
     return buffer
 
     
-print (download("https://youtube.com/shorts/TFmMP9sDMoc?si=H25yCmYZO8N1c1XB"))
